d3586048587c74891cc12ae3fa9e7d0c4a2124a4
Willigers <ronowilligers@gmail.com>
2024-07-17 16:28:39 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+
+var customerName = "bob";
+const favoriteCustomer = "bugaa";
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "bomboclat";
+}
